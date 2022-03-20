14286331b3462be60b95208e4b9d219f95c37f64
johan <alimjohan@gmail.com>
2022-03-21 06:18:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router,Route,Routes ,} from "react-router-dom";
-import NavBar from "./components/NavBar/NavBar.js";
 function App() {
   return (
     <Router>
